27e11146112b115e1e0afe1c84c4a41f08fc416c
Yoga <Yoga@DESKTOP-ET97QBD>
2026-08-18 13:02:20 +0300

    Add project files.

---
+++ socket/Program.cs
@@ -0,0 +1,51 @@
+﻿SuperParallel.Invoke(
+    () =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Task 1");
+    },
+
+    () =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Task 2");
+    },
+
+    () =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Task 3");
+    }
+);
+
+Console.WriteLine("All finished");
+
+Console.ReadLine();
+
+Console.WriteLine("\n================\n");
+
+
+SuperParallel.For(0, 10, i =>
+{
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+});
+
+Console.WriteLine("For finished");
+
+Console.WriteLine("\n================\n");
+
+
+
+List<int> numbers = new List<int>()
+{
+    1, 2, 3, 4, 5
+};
+
+SuperParallel.ForEach(numbers, n =>
+{
+    Console.WriteLine(n);
+    Thread.Sleep(1000);
+});
+
+Console.WriteLine("ForEach finished");
---
+++ socket/SuperParallel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+
+public static class SuperParallel
+{
+    public static void Invoke(params Action[] actions)
+    {
+        List<Task> tasks = new List<Task>();
+
+        foreach (Action action in actions)
+        {
+            Task task = new Task(action);
+            tasks.Add(task);
+            task.Start();
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    public static void For(int from, int to, Action<int> action)
+    {
+        List<Task> tasks = new List<Task>();
+
+        for (int i = from; i < to; i++)
+        {
+            int index = i;
+
+            Task task = new Task(() =>
+            {
+                action(index);
+            });
+
+            tasks.Add(task);
+            task.Start();
+        }
+
+        Task.WaitAll(tasks);
+    }
+
+    public static void ForEach<T>(IEnumerable<T> collection, Action<T> action)
+    {
+        List<Task> tasks = new List<Task>();
+
+        foreach (T item in collection)
+        {
+            T currentItem = item;
+
+            Task task = new Task(() =>
+            {
+                action(currentItem);
+            });
+
+            tasks.Add(task);
+            task.Start();
+        }
+
+        Task.WaitAll(tasks);
+    }
+}
